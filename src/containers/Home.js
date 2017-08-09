057bf884f8bec299a783aaf5ce2b337654f342c8
Stephen Van Gordon <stephen.van.gordon@gmail.com>
2017-08-09 11:00:00 -0700

    split containers and components

---
+++ src/containers/Home.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import { Container, Divider, Dropdown, Grid, Header, Image, List, Menu, Segment } from 'semantic-ui-react'
+import axios from 'axios';
+
+export class Home extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    axios.get('/fakeData.json')
+      .then((resp) => {
+        this.setState({
+          chapters: resp.data.chapters,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    this.state = {
+      chapters: null,
+    };
+  }
+
+  render() {
+    console.log("this.state.chapters", this.state.chapters)
+    return (
+      <div>
+        <Container text style={{ marginTop: '7em' }}>
+          <Header as='h1'>Semantic UI React Fixed Template</Header>
+          <p>This is a basic fixed menu template using fixed size containers.</p>
+          <p>A text container is used for the main container, which is useful for single column layouts.</p>
+
+
+          {!this.state.chapters ? null :
+            this.state.chapters.map((chapter) => (
+              <div>
+                <p>{`Chapter ${chapter.chapterNumber}: ${chapter.titleText}`}</p>
+              </div>
+            ))
+          }
+        </Container>
+
+        <Segment
+          inverted
+          vertical
+          style={{ margin: '5em 0em 0em', padding: '5em 0em' }}
+        >
+          <Container textAlign='center'>
+            <Grid divided inverted stackable>
+              <Grid.Row>
+                <Grid.Column width={3}>
+                  <Header inverted as='h4' content='Group 1' />
+                  <List link inverted>
+                    <List.Item as='a'>Link One</List.Item>
+                    <List.Item as='a'>Link Two</List.Item>
+                    <List.Item as='a'>Link Three</List.Item>
+                    <List.Item as='a'>Link Four</List.Item>
+                  </List>
+                </Grid.Column>
+                <Grid.Column width={3}>
+                  <Header inverted as='h4' content='Group 2' />
+                  <List link inverted>
+                    <List.Item as='a'>Link One</List.Item>
+                    <List.Item as='a'>Link Two</List.Item>
+                    <List.Item as='a'>Link Three</List.Item>
+                    <List.Item as='a'>Link Four</List.Item>
+                  </List>
+                </Grid.Column>
+                <Grid.Column width={3}>
+                  <Header inverted as='h4' content='Group 3' />
+                  <List link inverted>
+                    <List.Item as='a'>Link One</List.Item>
+                    <List.Item as='a'>Link Two</List.Item>
+                    <List.Item as='a'>Link Three</List.Item>
+                    <List.Item as='a'>Link Four</List.Item>
+                  </List>
+                </Grid.Column>
+                <Grid.Column width={3}>
+                  <Header inverted as='h4' content='Footer Header' />
+                  <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+
+            <Divider inverted section />
+            <Image
+              centered
+              size='mini'
+              src='/logo.png'
+            />
+            <List horizontal inverted divided link>
+              <List.Item as='a' href='#'>Site Map</List.Item>
+              <List.Item as='a' href='#'>Contact Us</List.Item>
+              <List.Item as='a' href='#'>Terms and Conditions</List.Item>
+              <List.Item as='a' href='#'>Privacy Policy</List.Item>
+            </List>
+          </Container>
+        </Segment>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/containers/PageHeader.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Container, Divider, Dropdown, Grid, Header, Image, List, Menu, Segment } from 'semantic-ui-react'
+
+const PageHeader = () =>
+  <Menu fixed='top' inverted>
+    <Container>
+      <Menu.Item as='a' header>
+        <Image
+          size='mini'
+          src='/logo.png'
+          style={{ marginRight: '1.5em' }}
+        />
+        Project Name
+      </Menu.Item>
+      <Menu.Item as='a'>Home</Menu.Item>
+
+      <Dropdown item simple text='Dropdown'>
+        <Dropdown.Menu>
+          <Dropdown.Item>List Item</Dropdown.Item>
+          <Dropdown.Item>List Item</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Header>Header Item</Dropdown.Header>
+          <Dropdown.Item>
+            <i className='dropdown icon' />
+            <span className='text'>Submenu</span>
+            <Dropdown.Menu>
+              <Dropdown.Item>List Item</Dropdown.Item>
+              <Dropdown.Item>List Item</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown.Item>
+          <Dropdown.Item>List Item</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </Container>
+  </Menu>
+
+export default PageHeader
